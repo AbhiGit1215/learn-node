f72e18acc99ba7ed19bee9d470ff491084223737
AbhiGit1215 <abhishek.asr92@gmail.com>
2022-06-28 16:10:15 +0530

    2 files modified, 2 added

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-  //process.end();
-  //console.log(req.method);
-  const url = req.url;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Test</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>'
-    );
-    res.write('</html>');
-    res.end();
-  }
-});
-server.listen(3000);
+const routes = require('./routes');
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
+server.listen(2800);
