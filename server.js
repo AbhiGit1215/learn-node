8fb15b205dd75671647bbd403204ff87eab87b7c
AbhiGit1215 <abhishek.asr92@gmail.com>
2022-06-28 12:48:01 +0530

    1 file modified

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   //process.end();
-  console.log(req.method);
+  //console.log(req.method);
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Test</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>'
+    );
+    res.write('</html>');
+    res.end();
+  }
 });
 server.listen(3000);
