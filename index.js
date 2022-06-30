2da96c07e64d8f22a0ac45a936f5e8cec5653d9b
AbhiGit1215 <abhishek.asr92@gmail.com>
2022-06-30 15:24:08 +0530

    7 files modified, 1 added

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
